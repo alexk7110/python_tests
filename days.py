f8625b955d8d989b2c5be3d64f6ccee15d2d82fb
Alexandros Kastanis <alex.kastanis@gmail.com>
2022-06-25 12:57:25 +0300

    Update days.py

--- days.py
+++ days.py
@@ -3,5 +3,5 @@ from datetime import date as D
 print ("MyCloud lifetime",D.today() - D(2015, 8, 22))
 print ("Mate 8 lifetime",D.today() - D(2016, 3, 26))
 print ("Mint lifetime",D.today() - D(2016, 8, 29))
-print ("Pi Server lifetime",D.today() - D(2016, 11, 9))
+
 print ("Pi USB lifetime",D.today() - D(2017, 1, 10))
